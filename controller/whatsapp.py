f8263e5ea97cf3002f85516ab980d7511697bf7d
Dattaprasad Ekavade <dattaprasad@ajency.in>
2020-02-24 19:41:08 +0530

    fallback the old commit

--- controller/whatsapp.py
+++ controller/whatsapp.py
@@ -14,12 +14,13 @@ from google.cloud.language_v1 import enums, types
 from google.oauth2.service_account import Credentials
 from sqlalchemy import create_engine, MetaData, Table, Column, select
 
-engine = create_engine(serverUrl())
+account_sid = 'AC797feaab84bdd385bbb2ae0f1c08e8b6'
 
-connection = engine.connect()
-metadata = MetaData()
-twilioKey = Table('whatsapp_company_twilio_accounts', metadata,
-                  autoload=True, autoload_with=engine)
+with open('../twiliokey.json', 'r') as jsonfile:
+    data = jsonfile.read()
+
+obj = json.loads(data)
+auth_token = str(obj['key'])
 
 whatsapp_call = Blueprint('whatsapp', __name__)
 
@@ -57,15 +58,6 @@ def new_text():
 @whatsapp_call.route("/sms", methods=['GET', 'POST'])
 def incoming_sms():
     # Get the message the user sent our Twilio number
-    account_sid = request.values.get('AccountSid', None)
-
-    query = select([twilioKey.columns.auth_token]).where(twilioKey.columns.account_sid ==
-                                                         account_sid)
-    #
-    ResultProxy = connection.execute(query)
-
-    ResultSet = ResultProxy.fetchone()
-    auth_token = ResultSet[0]
 
     print(vars(request.values))
     body = request.values.get('Body', None)
